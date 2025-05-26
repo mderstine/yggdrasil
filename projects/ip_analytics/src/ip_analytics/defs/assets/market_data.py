f07bcd05696b38bc40454344991bef2c4b2a9c74
MD <derstine.michael@gmail.com>
2025-05-25 23:31:32 -0500

    almost working docker compose build

---
+++ projects/ip_analytics/src/ip_analytics/defs/assets/market_data.py
@@ -0,0 +1,31 @@
+import dagster as dg
+import httpx
+import polars as pl
+
+
+@dg.asset
+def sofr(context: dg.AssetExecutionContext) -> dg.MaterializeResult:
+
+    fred_api_key = "41d1cc1b88629e3431c4d2e39bffc925"
+    url = f'https://api.stlouisfed.org/fred/series/observations'
+    params = {
+        "series_id": "SOFR",
+        "api_key": fred_api_key,
+        "file_type": "json"
+    }
+
+    response = httpx.get(url, params=params)
+    data = response.json()["observations"]
+
+    sofr = pl.DataFrame(
+        {
+            "date": [item["date"] for item in data],
+            "value": [
+                float(item["value"]) if item["value"] else None for item in data
+            ]
+        }
+    )
+
+    context.log.debug(sofr)
+
+    return dg.MaterializeResult()
